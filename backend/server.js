80df9e8a0a8e55bd67c3be3fa7ef6d9cb1e213ac
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-25 23:27:52 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://credit-card-checker-psi.vercel.app/",
+      "https://credit-card-checker-psi.vercel.app",
     ],
     credentials: true,
   })
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_API_BASE_URL || "http://localhost:4001/api",
+  baseURL: process.env.NEXT_API_BASE_URL,
   withCredentials: true,
 });
 
