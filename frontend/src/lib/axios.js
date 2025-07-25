c87839328a747b4d85e1b172d27fa655e76406ed
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-26 00:01:01 +0530

    check once

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://credit-card-checker.onrender.com/api",
   withCredentials: true,
 });
 
